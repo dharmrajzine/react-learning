ac1f8bb9e405cbceb165ec90455c1c81252501b7
dharmrajzine <dharmrajzine@gmail.com>
2024-06-18 16:01:07 +0530

    Mega-Blog

---
+++ 12-MegaBlogWithAppwrite/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  console.log();
+  return (
+    <>
+      <h1>A blog appp with appwrite</h1>
+    </>
+  );
+}
+
+export default App;
